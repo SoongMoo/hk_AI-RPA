df2379f66a686c282d482c839e190f1c89e7e25f
HKEDU <HKEDU@DESKTOP-ON7PE9G.mshome.net>
2023-04-04 11:46:07 +0900

    로그인

--- hkAiRpaProject/src/main/java/hkAiRpaProject/contoller/LoginController.java
+++ hkAiRpaProject/src/main/java/hkAiRpaProject/contoller/LoginController.java
@@ -2,6 +2,7 @@ package hkAiRpaProject.contoller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import hkAiRpaProject.command.LoginCommand;
@@ -13,8 +14,8 @@ public class LoginController {
 	@Autowired
 	LoginService loginService;
 	@RequestMapping("/login/loginPro")
-	public String login( LoginCommand loginCommand, HttpSession session) {
-		loginService.execute(loginCommand, session);
+	public String login( LoginCommand loginCommand, BindingResult result , HttpSession session) {
+		loginService.execute(loginCommand, session, result);
 		return "redirect:/";
 	}
 	@RequestMapping("/login/loginOut")
--- hkAiRpaProject/src/main/java/hkAiRpaProject/service/login/LoginService.java
+++ hkAiRpaProject/src/main/java/hkAiRpaProject/service/login/LoginService.java
@@ -1,7 +1,9 @@
 package hkAiRpaProject.service.login;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
 
 import hkAiRpaProject.command.LoginCommand;
 import hkAiRpaProject.domain.AuthInfoVO;
@@ -12,8 +14,26 @@ import jakarta.servlet.http.HttpSession;
 public class LoginService {
 	@Autowired
 	LoginMapper loginMapper;
-	public void execute(LoginCommand loginCommand, HttpSession session) {
+	@Autowired
+	PasswordEncoder passwordEncoder;
+	public void execute(LoginCommand loginCommand, HttpSession session,BindingResult result) {
 		AuthInfoVO vo = loginMapper.loginSelect(loginCommand.getUserId());
-		if (vo != null) session.setAttribute("authInfo", vo); ///접속중인 사용자입니다.
+		if (vo != null) {
+			System.out.println("가입된 회원입니다.");
+			if (vo.getGrade().equals("mem") && vo.getMemberOk()==null) {
+				System.out.println("이메일인증이 안된 회원");
+				result.rejectValue("userId", "loginCommand.userId", "이메일 인증이 안되었습니다.");
+			}
+			if(passwordEncoder.matches(loginCommand.getUserPw(), vo.getUserPw())) {
+				System.out.println("비밀번호가 일치");
+				session.setAttribute("authInfo", vo);///접속중인 사용자입니다
+			}else {
+				System.out.println("비밀번호가 일치 하지 않습니다.");
+				result.rejectValue("userPw", "loginCommand.userPw", "비밀번호가 틀렸습니다.");
+			}
+		}else{
+			System.out.println("가입하지 않은 회원입니다.");
+			result.rejectValue("userId", "loginCommand.userId", "아이디가 없습니다.");
+		}
 	}
 }
